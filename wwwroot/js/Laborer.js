9dfb97fdfafaec9e3cf0b5eafe655d79342af499
ssskram <paul.marks@pittsburghpa.gov>
2018-01-24 17:31:59 -0500

    Can't get prev until to work on repeating elements

--- wwwroot/js/Laborer.js
+++
@@ -1,7 +0,0 @@
-function CheckField () {
-    var accepted = $('#accepted').val();
-    var called = $('#called').val();
-    if ( (accepted == "Yes" | accepted == "No") & (called == "Yes" ) ) {
-        $('#trigger').parent().parent().remove();
-    }
-}
