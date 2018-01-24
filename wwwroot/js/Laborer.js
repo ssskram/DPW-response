e5cd2b8a3d023392b34bfa8fe31a94ccc25d0096
ssskram <paul.marks@pittsburghpa.gov>
2018-01-24 15:54:54 -0500

    responsive bootstrap-select workin

--- wwwroot/js/Laborer.js
+++ wwwroot/js/Laborer.js
@@ -1,13 +1,7 @@
 function CheckField (){
     var accepted = $('#accepted').val();
-    if ( (accepted == "Yes" | accepted == "No") & ($('#called').is(':checked')) ) {
-
-        $("#button").prop("disabled",false);
-        $("#checkmark").css("display", "block");
-
-    } else {
-
-        $("#button").prop("disabled",true);
-        $("#checkmark").css("display", "none");
+    var called = $('#called').val();
+    if ( (accepted == "Yes" | accepted == "No") & (called == "Yes" ) ) {
+        $('#trigger').parent().parent().remove();
     }
 }
