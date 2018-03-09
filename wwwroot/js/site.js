2a004bf299e0edca31e2dfce8307759f334603cc
ssskram <paul.marks@pittsburghpa.gov>
2018-03-09 12:48:50 -0500

    added overlay loader, centered login page and got rid of footer

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,3 +3,13 @@ function textAreaAdjust(o) {
     o.style.height = "1px";
     o.style.height = (25+o.scrollHeight)+"px";
     }
+
+// show overlay spinner on network activity
+// just add class "overlaytrigger" to buttons for this to pop off
+var classname = document.getElementsByClassName('overlaytrigger');
+var overlay = function() {
+    document.getElementById('overlayloader').style.display = 'flex';
+};
+Array.from(classname).forEach(function(element) {
+    element.addEventListener('click', overlay);
+  });
